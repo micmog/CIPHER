b2e10276d5567e1387930f2271e315f623e7b345
Gerard Capes <gerard.capes@manchester.ac.uk>
2026-02-04 14:46:10 +0000

    Update installation instructions
    - Drop `--with-cxx-dialect=C++11` configure option
    - Use gcc compiler
    - Add installation script and slurm jobscript for CSF3
    - Update README to use those new scripts

---
+++ install-jobscript.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#SBATCH -p multicore
+#SBATCH -n 2
+#SBATCH -t 60:00
+
+source ./install.sh
---
+++ install.sh
@@ -0,0 +1,17 @@
+export CIPHER_DIR=/mnt/eps01-rds/jf01-home01/shared/CIPHER
+export PETSC_DIR=$CIPHER_DIR/petsc
+export PETSC_ARCH=cipher
+
+module load mpi/gcc/openmpi/4.1.8-gcc-14.2.0
+
+cd $PETSC_DIR
+./configure --download-metis --download-parmetis --download-chaco \
+  --download-triangle --download-ctetgen --download-pragmatic \
+  --download-eigen --download-hypre --download-ml --download-hdf5 \
+  --download-zlib --download-yaml --download-p4est --with-pthread \
+  --with-debugging=0  --download-fblaslapack=1
+make all 2>&1 | tee make-all.log
+make check 2>&1 | make-check.log
+
+cd $CIPHER_DIR
+make install 2>&1 | tee make-install.log
